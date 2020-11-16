c7138f06d4bf3d684c6170f0ba9c6315834c06c0
McMaartenz <mcmaartenz@outlook.com>
2020-11-16 23:10:50 +0100

    memory management

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 echo "Merging.."
+
 cat \
 ../boot/bootloader.vsb \
 ../boot/main.vsb \
 ../boot/kernel.vsb \
 ../boot/addblck.vsb \
-../boot/addblck1.vsb \
+../boot/rmblck.vsb \
 > script.txt
 
 echo "Removing leading whitespace.."
+
 sed "s/^[ \t]*//" -i script.txt
+unix2dos script.txt
 
 echo "Done."
